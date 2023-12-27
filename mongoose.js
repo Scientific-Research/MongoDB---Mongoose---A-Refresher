cdfc81606c04d9223b3144fc12ef3918bbce5267
Maximilian <scientificresearch309@gmail.com>
2023-12-27 18:18:43 +0100

    Creating a Product according to the model that was created already and not MongoClient

---
+++ mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const Product = require("./models/product");
+
+module.exports.createProduct = async((req, res, next) => {
+  const createdProduct = new Product({
+    name: req.body.name,
+    price: req.body.price,
+  });
+});
