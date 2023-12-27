8bc328637711662a9f9f9623ddf950b8ca656f90
Maximilian <scientificresearch309@gmail.com>
2023-12-27 13:23:43 +0100

    Creating a Simple backend & Connecting it to the Database

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const { createProduct, getProducts } = require("./mongo");
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post("/products");
+app.post("/products", createProduct);
 
-app.get("/products");
+app.get("/products", getProducts);
 
 app.listen(3000, () => {
   console.log("Connected to the Server successfully!");
---
+++ mongo.js
@@ -0,0 +1,8 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const url =
+  "mongodb+srv://Maximilian:WCzptjAhAg2RNAEE@cluster0.ki7w2ay.mongodb.net/products_test?retryWrites=true&w=majority";
+
+module.exports.createProduct = async (req, res, next) => {};
+
+module.exports.getProducts = async (req, res, next) => {};
